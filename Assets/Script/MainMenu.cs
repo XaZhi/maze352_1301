f7a9bc8e5c73ad53d668b19d2f7737cbae448097
MatepiyaTK <Matepiya.n@gmail.com>
2026-09-17 03:57:49 +0700

    Loading

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Scene01");
+        SceneManager.LoadScene("Loading");
     }
 
     public void ExitGame()
